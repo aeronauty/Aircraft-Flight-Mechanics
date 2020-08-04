fc0f2591d7369d67420a6133fba9d3904c302e0a
aeronauty <smith.harry@gmail.com>
2020-08-04 11:39:22 -0500

    Intro Changed

--- _build/jupyter_execute/NotesIntroduction.py
+++ _build/jupyter_execute/NotesIntroduction.py
@@ -1,24 +1,49 @@
 
-# Introduction to this content
+# Introduction to this website
 
-This website comprises the course notes for MMAE 410 Aircraft Flight Mechanics - this text originally started as a PDF file written using LaTeX, with links to code and other tidbits. These notes started as my means of ensuring my own competence with the material and, accordingly, they get updated regularly and requiring students to download an updated PDF file every few weeks proved to not be the ideal solution.
+This website comprises the course notes for MMAE 410 Aircraft Flight Mechanics at the Illinois Institute of Technology - this text originally started as a PDF file written using LaTeX, with links to code and other tidbits. These notes started as my means of ensuring my own competence with the material and, accordingly, they get updated regularly and requiring students to download an updated PDF file every few weeks proved to not be the ideal solution.
 
 So now these notes are being transferred into Jupyter notebooks which are bundled together into this document - any changes made to the notes should be reflected in the website within minutes, and this format allows handy things like _interactive content,_ which simply isn't possible (easily) within a PDF.
 
 It does mean that my pipedream of getting them published as a textbook is over - but in the spirit of open access and education for all, I'd simply rather that they just got used for their intended purpose. Hence; use these notes to supplement your own learning, and to further your knowledge of the discipline.
 
-## Interactive content - using Python
+## Why use Python
 
-The original PDF notes were written and contained examples using both MATLAB and Python. Over the past few months, I've made the personal decision to switch _all_ my code from MATLAB to Python. There are many reasons for this change, but the benefit for the reader is that you can view the source for any of the plots included in this book. There should also be interactive content that will help you to explore equations, and understand the relationship between different parameters.
+The original PDF notes were written and contained examples using both MATLAB and Python. Over the past few months, I've made the personal decision to switch _all_ my code from MATLAB to Python. There are many reasons for this change, but the benefit for the reader is that you can view the source for any of the plots included in this book without purchasing a MATLAB license. 
 
-Here's a trivial example of a plot that you can edit
+### Interactive content
 
+There is iteractive content that will help you to explore equations, and understand the relationship between different parameters.
 
-And see {eq}`my_label` below:
-```{math}
-:label: my_label
-F = m\,a
+The way this works is slightly convoluted, but very powerful - this whole book is hosted as a GitHub repository, as a mixture of ```.md``` and ```.ipynb``` files. This means that any Jupyter Notebooks can be run on the web thanks to the kind folks at DockerHub. The book is written using JupyterBook, which is a plugin called *thebe*, which enables the code sections to be run without having to actually go to DockerHub.
+
+What this means is that you have a few different ways to access the interactive content:
+- Download the whole ```.ipynb``` file and run natively in Juypter on your machine: {ref}`Github`
+- Run the notebook in Docker: {ref}`Binder`
+- Run the code in-place using thebe: {ref}`Thebe`
+
+Both of the latter work well on the pages I have tested - but it can take time for the whole notebook to be initlialised. If you click on "run code" in the dropdown from the rocket icon at the top of the page, it needs to connect to Docker, which then connects to GitHub, and then send the content back to this site.
+
+```{figure} Images/Binder.png
+---
+height: 30px
+name: Binder
+---
+Connecting to Binder/Thebe
 ```
+
+```{figure} Images/Github.png
+---
+height: 30px
+name: Github
+---
+Downloading the source or a PDF
+```
+
+Give it a go, though - here's a trivial example of a plot that you can edit. Try the different ways of accessing this content.
+
+The plot below plots $y=\sin(n\cdot x)$ in the interval 0 to $2\pi$. You can change whatever you want in the code below and make it run.
+
 
 
 
